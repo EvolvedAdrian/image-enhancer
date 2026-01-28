0a2d27c2e7d7b433216a246e84ade21a10c56c78
HelloAdrian2k <mz.adriaan@gmail.com>
2026-01-28 09:36:48 +0100

    feat: add scratch repairing functionality

--- src/models.py
+++ src/models.py
@@ -1,12 +1,30 @@
 from gfpgan import GFPGANer
 from config import CONFIG
+from zeroscratches import EraseScratches
+from src.utils import bgr_to_pil, pil_to_bgr
+import streamlit as st
 
 class ImageEnhancer:
     """Class to manage GFPGAN model and image processing"""
     def __init__(self):
         self.model = None
+        self._scratch_eraser = None
 
-    def load_model_simple(self):
+    @property
+    def scratch_eraser(self):
+        if self._scratch_eraser is None:
+            self._scratch_eraser = EraseScratches()
+        return self._scratch_eraser
+    
+    def _remove_scratches(self, img_bgr):
+        """Remove stripes and scratches from image before passing into GFPGAN"""
+        pil_img = bgr_to_pil(img_bgr)
+        restored_rgb = self.scratch_eraser.erase(pil_img)
+        restored_bgr = pil_to_bgr(restored_rgb)
+        return restored_bgr
+
+    @st.cache_resource
+    def load_model_simple(_self):
         """Loads GFPGAN model for face enhancement"""
         model = GFPGANer(
             model_path=CONFIG["model_url"],
@@ -17,12 +35,16 @@ class ImageEnhancer:
         )
         return model
     
-    def enhance(self, image_bgr):
+    def enhance(self, image_bgr, repair_scratches=False):
         """Enhances an image with customizable options
 
         Args:
             image_bgr: Image in BGR format
         """
+        if repair_scratches:
+            st.info("Reparando grietas y arañazos...")
+            image_bgr = self._remove_scratches(image_bgr)
+        
         if not self.model:
             self.model = self.load_model_simple()
         
